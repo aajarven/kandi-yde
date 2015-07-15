988b5be646ed5362be68d4d241e4448fb36725ab
aajarven <anni.jarvenpaa@gmail.com>
2015-07-15 16:16:32 +0300

    koodi lyhyemmäksi ja simppelimmäksi, ei kommentteja

--- python/volume-spiraali.py
+++ python/volume-spiraali.py
@@ -8,12 +8,9 @@ ds = yt.load("/data/scratch3/extragal/Enzo/data/LWIR/RD0076/RD0076")
 ad = ds.all_data()
 
 v, tiheysmaksimi = ds.find_max("density")
-#print tiheysmaksimi
 densSphere = ds.sphere(tiheysmaksimi, (0.5, "kpc"))
 maxlocs = densSphere.quantities.max_location(("gas", "H2_fraction"))
-#print maxlocs
 H2max = maxlocs[2:5]
-#print H2max
 H2sphere = ds.sphere(H2max, (1.5, "kpc"))
 
 mi, ma = H2sphere.quantities.extrema("H2_fraction")
@@ -32,6 +29,5 @@ frames = int(math.floor(2*math.pi/step))
 
 for i, snapshot in enumerate(cam.zoomin(7, frames, clip_ratio=8.0)):
     cam.rotate(0.05)
-    #snapshot.write_png("../kuvat/volume/spiraali/H2_frac/H2frac-%04i.png" % frame, clip_ratio=8.0)
-    cam.save_annotated("../kuvat/volume/spiraali/H2_frac/1500pc/%04i.png" %frame, snapshot)
+    snapshot.write_png("kuvat/%04i.png" %frame)
     frame += 1
